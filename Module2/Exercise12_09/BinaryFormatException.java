4248af5b8990feb9e55242c3680a0d5bc9131a9a
AldoJCL <157396844+AldoJCL@users.noreply.github.com>
2026-05-09 21:16:35 +0000

    Complete Exercise12_09 binary format exception

---
+++ Module2/Exercise12_09/BinaryFormatException.java
@@ -0,0 +1,10 @@
+package Module2.Exercise12_09;
+
+public class BinaryFormatException extends Exception {
+    // Basic custom exception for invalid binary strings
+    public BinaryFormatException(String message) {
+        super(message);
+    }
+}
+    
+
